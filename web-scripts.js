adb3f369f355ee2b45603aa30bdd55b2ef06355e
Raiyan Mohammad Rahman <raiyanmohammadrahman@protonmail.com>
2026-05-29 16:57:21 -0400

    w3.css + js test

---
+++ web-scripts.js
@@ -0,0 +1,22 @@
+/* 
+Raiyan Mohammad Rahman & Talha Nadeem
+btech-engl2026-web scripts
+sourced from w3schools
+*/
+function useAccordion(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function openTab(id) {
+  var i;
+  var x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(id).style.display = "block";
+}
